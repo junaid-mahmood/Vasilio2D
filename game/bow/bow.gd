e5e3d660412f555035ce7b90aae567cb77ca4392
Magicninja7 <j.t.ptaszek@gmail.com>
2025-02-14 22:33:38 +0100

    bow + sword + bugfixes

---
+++ game/bow/bow.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+
+func _process(delta):
+	position.y += sin(Time.get_ticks_msec() / 200) * delta * 80
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if "has_gun" in body:
+		body.has_gun = true
+		queue_free()
--- game/main.gd
+++ game/main.gd
@@ -2,6 +2,7 @@ extends Node
 
 const bullet_scene: PackedScene = preload("res://character/bullet.tscn")
 const enemy_bullet_scene: PackedScene = preload("res://enemy/enemy_bullet.tscn")
+const bullet_bow_scene: PackedScene = preload("res://bow/bow_bullet.tscn")
 
 
 
@@ -25,3 +26,13 @@ func _on_enemy_enemy_shoot(pos: Variant, player_pos: Variant) -> void:
 	bullet.velocity = direction * bullet.speed
 	bullet.rotation = direction.angle()
 	$EnemyBullets.add_child(bullet)
+
+
+
+func _on_character_body_2d_shoot_bow(pos: Variant, facing_right: Variant) -> void:
+	var bullet = bullet_bow_scene.instantiate()
+	var direction = 1 if facing_right else -1
+	bullet.direction = direction
+	$BowBullets.add_child(bullet)
+	pos.y -= 20
+	bullet.position = pos + Vector2(6 * direction, 0)
